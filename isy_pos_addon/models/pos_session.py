1d728bd57cd0a8779fa7d88560f04c46a047f29b
Thae Ei Htwe <thtwe@isyedu.org>
2026-03-31 09:08:33 +0630

    Added override function for POS order minus amount.

--- isy_pos_addon/models/pos_session.py
+++ isy_pos_addon/models/pos_session.py
@@ -72,3 +72,28 @@ class PosSession(models.Model):
             'fields': ['name', 'is_cash_count', 'use_payment_terminal', 'split_transactions', 'type', 'image', 'sequence', 'use_card'],
             },
         }
+
+    def _create_split_account_payment(self, payment, amounts):
+        payment_method = payment.payment_method_id
+        if not payment_method.journal_id:
+            return self.env['account.move.line']
+        outstanding_account = payment_method.outstanding_account_id or self.company_id.account_journal_payment_debit_account_id
+        accounting_partner = self.env["res.partner"]._find_accounting_partner(payment.partner_id)
+        destination_account = accounting_partner.property_account_receivable_id
+
+        # if float_compare(amounts['amount'], 0, precision_rounding=self.currency_id.rounding) < 0:
+        #     # revert the accounts because account.payment doesn't accept negative amount.
+        #     outstanding_account, destination_account = destination_account, outstanding_account
+
+        account_payment = self.env['account.payment'].create({
+            'amount': abs(amounts['amount']),
+            'partner_id': accounting_partner.id,
+            'journal_id': payment_method.journal_id.id,
+            'force_outstanding_account_id': outstanding_account.id,
+            'destination_account_id': destination_account.id,
+            'ref': _('%s POS payment of %s in %s', payment_method.name, payment.partner_id.display_name, self.name),
+            'pos_payment_method_id': payment_method.id,
+            'pos_session_id': self.id,
+        })
+        account_payment.action_post()
+        return account_payment.move_id.line_ids.filtered(lambda line: line.account_id == account_payment.destination_account_id)
